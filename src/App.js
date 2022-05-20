612fc75d84d4ab0aa8e1ad48eac10fa693b19c00
DarkS <sk8.merp@gmail.com>
2022-05-20 14:17:44 -0300

    20/02/2022

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './Components/Header/';
+import Footer from './Components/Footer/';
+import Home from './Components/Home/';
+import Login from './Components/Login/login';
+import { UserStorage } from './UserContext';
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <UserStorage>
+          <Header/>
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/login/*' element={<Login/>}/>
+            </Routes>
+          <Footer/>
+        </UserStorage>
+      </BrowserRouter>
+    </div>
   );
 }
 
